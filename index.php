c483065041f736842b7f79981492ed020b0ef6b7
Joshua Garcia <j.garcia12998@gmail.com>
2015-05-27 09:07:13 -0700

    style 1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<header>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+  <nav>
+    <a href="#">CONTACT ME</a>
+  </nav>
+	<title></title>
+</header>
+<div class="box">
+  <h3> meet</h3>
+  <div class="container">
+    <div class="box-child">
+      <img src="images/me.jpg">
+    </div>
+    <div class="box-child">
+      <p>Me, Joshua Garcia. Front end devevloper in process</p>
+    </div>
+  </div>
+</div>
+<body>
+</body>
+</html>
+
+
+
+
+
